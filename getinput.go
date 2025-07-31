2a17a583327cfdedec7c6aecb481690de365799f
Vinay Chandrasekhar <vinayc.11b.kmbvm@gmail.com>
2025-07-31 20:37:02 +0530

    blah

--- getinput.go
+++ getinput.go
@@ -1,39 +1,67 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "golang.org/x/term"
+	"fmt"
+	"os"
+	"strings"
+
+	"golang.org/x/term"
 )
-func HandleCompletion(cmd string) string{
-    
-}
 
-func ReadInput()(string,error){
-    fd := int(os.Stdin.Fd())
-    old,_ := term.MakeRaw(fd)
-    defer term.Restore(fd,old)
-    buf := make([]byte, 1)
-    cmd := ""
-    for{
-	_,err := os.Stdin.Read(buf)
-	if err != nil {
-	    return "",err
-	} else if buf[0] == 9{
-	    HandleCompletion(cmd)
-	} else if buf[0] == 127{
-	    if (len(command) > 0){
-		command = command[:len(command)-1]
-	    }
-	}else if buf[0] == 3{
-	    command = command + "^C"
-	    break
-	}else {
-	    command = command + string(buf)
+var commands = []string{"exit", "echo"}
+
+
+func HandleCompletion(cmd string) string {
+	for _, c := range commands {
+		fmt.Println(c)
+		if strings.HasPrefix(c, cmd) {
+
+			return c
+		}
 	}
-	fmt.Print("\x1b[2K\x1b[1G" + command)
-    }
-    return cmd,nil
+	return ""
 }
 
+func ReadInput() (string, error) {
+	fd := int(os.Stdin.Fd())
+	oldState, err := term.MakeRaw(fd)
+	if err != nil {
+		return "", err
+	}
+	defer term.Restore(fd, oldState)
+
+	buf := make([]byte, 1)
+	cmd := ""
+
+	for {
+		_, err := os.Stdin.Read(buf)
+		if err != nil {
+			return "", err
+		}
+
+		switch buf[0] {
+		case 9: // Tab key
+		fmt.Println("working")
+			completed := HandleCompletion(cmd)
+			if completed != "" {
+				cmd = completed
+			}
+		case 127: // Backspace
+			if len(cmd) > 0 {
+				cmd = cmd[:len(cmd)-1]
+			}
+		case 3: // Ctrl+C
+			fmt.Println("^C")
+			return "", fmt.Errorf("interrupted")
+		case 13: // Enter
+			fmt.Println()
+			return cmd, nil
+		default:
+			cmd += string(buf[0])
+		}
+
+		// Clear and redraw line
+		fmt.Print("\x1b[2K\r" + cmd)
+	}
+}
 
